63da83d6e15a5f28ea0b3c588734fb8262f43d80
Thiago Senna <trsenna@gmail.com>
2019-08-21 11:39:12 -0300

    added with apologies only

--- dia-06/src/components/Alert/Apologize/index.js
+++ dia-06/src/components/Alert/Apologize/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
-const ApologizeAlert = props => {
-    const { apologies } = props;
-
+const withApologiesOnly = ( Component ) => ( { apologies, ...rest } ) => {
     return ( apologies && apologies.length > 0 )
-        ? <div className="alert alert-warning">{ apologies }</div>
+        ? <Component apologies={ apologies } { ...rest }/>
         : null;
 };
 
-export default ApologizeAlert;
+const ApologizeAlert = props => {
+    const { apologies } = props;
+
+    return (
+        <div className="alert alert-warning">
+            { apologies }
+        </div>
+    );
+};
+
+export default withApologiesOnly( ApologizeAlert );
